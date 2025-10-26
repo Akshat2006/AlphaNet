cf150ded499cde5ae186adc1e894303ec387f97e
Akshat Ranjan <akshatranjan910@gmail.com>
2025-10-26 10:11:47 +0530

    idk random bs

--- neuralnetwork.h
+++ neuralnetwork.h
@@ -19,6 +19,8 @@ public:
 	neural_net(std::vector<LAYER>& architecture, double learning_rate_ = 0.01);
 	const VECTOR& forward(const VECTOR& input);
 	void backward(VECTOR& input, VECTOR& target);	
+	double train(const VECTOR& input, const VECTOR& target);
+	size_t predict(const VECTOR& input);
 	
 };
 
